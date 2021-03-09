0b62c56a1f4bea54f1609d2e18282c1919afae19
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 10:10:47 +0100

    Add project files.

---
+++ PETDataBase.WPF/Models/ObservableObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PETDataBase.WPF.Models
+{
+    /// <summary>
+    /// Implements INotifyPropertyChanged
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Notifies that <paramref name="propname"/> has changed
+        /// </summary>
+        /// <param name="propname">
+        /// Updates databindings with this propname
+        /// </param>
+        protected void OnPropertyChanged(string propname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+    }
+}
---
+++ PETDataBase.WPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using PETDataBase.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PETDataBase.WPF.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
