810444d018d0c8f24bf7d0f5d97783d8854a1571
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 10:33:25 +0100

    Adds Person model

---
+++ PETDataBase.Domain/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class Person : DomainObject
+    {
+        #region Properties
+
+        // Base info
+
+        public string FullName { get; set; }
+        public string Adress { get; set; }
+        public List<string> KeyWords { get; set; }
+
+
+        // Apperance
+
+        public float Height { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+        public string SkinColor { get; set; }
+        public string Ethnicity { get; set; }
+        // To be replaced with Nationality model
+        public object Nationality { get; set; }
+        public Bitmap Photo { get; set; }
+
+
+
+        #endregion
+    }
+}
