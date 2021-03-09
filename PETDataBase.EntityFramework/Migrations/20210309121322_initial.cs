695fed52312f1308b692f8973db874ce562908c0
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 13:36:08 +0100

    Revert "Revert "Adds initial migration""
    This reverts commit 70032e5800b8aafb7ea106c3fc01517358790bf3.

---
+++ PETDataBase.EntityFramework/Migrations/20210309121322_initial.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PETDataBase.EntityFramework.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nationalities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CountryCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalityName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nationalities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentMethods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentDescription = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentMethods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Informants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentMethodId = table.Column<int>(type: "int", nullable: true),
+                    CurrencyId = table.Column<int>(type: "int", nullable: true),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Height = table.Column<float>(type: "real", nullable: true),
+                    EyeColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HairColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SkinColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ethnicity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NationalityId = table.Column<int>(type: "int", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Informants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Informants_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Informants_Nationalities_NationalityId",
+                        column: x => x.NationalityId,
+                        principalTable: "Nationalities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Informants_PaymentMethods_PaymentMethodId",
+                        column: x => x.PaymentMethodId,
+                        principalTable: "PaymentMethods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    LastEdit = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReportId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Informants_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Informants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Keyword",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KeyWord = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InformantId = table.Column<int>(type: "int", nullable: true),
+                    ObservantId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Keyword", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Keyword_Informants_InformantId",
+                        column: x => x.InformantId,
+                        principalTable: "Informants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateSubmitted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: true),
+                    SubjectId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reports_Informants_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Informants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Observants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GroupId = table.Column<int>(type: "int", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Height = table.Column<float>(type: "real", nullable: false),
+                    EyeColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HairColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SkinColor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ethnicity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NationalityId = table.Column<int>(type: "int", nullable: true),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Observants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Observants_Nationalities_NationalityId",
+                        column: x => x.NationalityId,
+                        principalTable: "Nationalities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ObservantId = table.Column<int>(type: "int", nullable: true),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subject_Observants_ObservantId",
+                        column: x => x.ObservantId,
+                        principalTable: "Observants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId",
+                table: "Comments",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ReportId",
+                table: "Comments",
+                column: "ReportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Informants_CurrencyId",
+                table: "Informants",
+                column: "CurrencyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Informants_NationalityId",
+                table: "Informants",
+                column: "NationalityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Informants_PaymentMethodId",
+                table: "Informants",
+                column: "PaymentMethodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Keyword_InformantId",
+                table: "Keyword",
+                column: "InformantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Keyword_ObservantId",
+                table: "Keyword",
+                column: "ObservantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Observants_GroupId",
+                table: "Observants",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Observants_NationalityId",
+                table: "Observants",
+                column: "NationalityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_AuthorId",
+                table: "Reports",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_SubjectId",
+                table: "Reports",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subject_ObservantId",
+                table: "Subject",
+                column: "ObservantId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Reports_ReportId",
+                table: "Comments",
+                column: "ReportId",
+                principalTable: "Reports",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Keyword_Observants_ObservantId",
+                table: "Keyword",
+                column: "ObservantId",
+                principalTable: "Observants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reports_Subject_SubjectId",
+                table: "Reports",
+                column: "SubjectId",
+                principalTable: "Subject",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Observants_Subject_GroupId",
+                table: "Observants",
+                column: "GroupId",
+                principalTable: "Subject",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Observants_Nationalities_NationalityId",
+                table: "Observants");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subject_Observants_ObservantId",
+                table: "Subject");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Keyword");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "Informants");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "PaymentMethods");
+
+            migrationBuilder.DropTable(
+                name: "Nationalities");
+
+            migrationBuilder.DropTable(
+                name: "Observants");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+        }
+    }
+}
