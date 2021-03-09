0bb00c7fec129ed05debe8101e8a521d913c30ec
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 11:51:25 +0100

    Adds PaymentMethod/Currency models

---
+++ PETDataBase.Domain/Models/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class Currency : DomainObject
+    {
+        #region Properties
+        public string CurrencyName { get; set; }
+        #endregion
+    }
+}
---
+++ PETDataBase.Domain/Models/PaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class PaymentMethod : DomainObject
+    {
+        #region Properties
+        public string PaymentDescription { get; set; }
+        #endregion
+    }
+}
