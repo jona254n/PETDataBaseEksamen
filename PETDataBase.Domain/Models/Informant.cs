67d310b2fed927ce2c1accf3083e49226d773c4c
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 10:54:04 +0100

    Adds Informant Model

---
+++ PETDataBase.Domain/Models/Informant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class Informant
+    {
+        public Informant(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+        #region Properties
+        public string PaymentMethod { get; set; }
+        public string Currency { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        #endregion
+    }
+}
