d26119dc690daeffaa522f85631a34518f8797a5
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 11:15:56 +0100

    Adds report class

---
+++ PETDataBase.Domain/Models/Report.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class Report : DomainObject
+    {
+        #region Properties
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DateSubmitted { get; set; }
+
+        public Informant Author { get; set; }
+
+        //To be replaced 
+        public object Subject { get; set; }
+        //To be replaced
+        public List<object> Comments { get; set; }
+        #endregion
+    }
+}
