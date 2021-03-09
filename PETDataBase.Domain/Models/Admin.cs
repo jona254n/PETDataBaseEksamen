c010940a561bb8ee31df612d92ac4899326f9e5a
jona254n <jona254n@edu.campusvejle.dk>
2021-03-09 11:41:26 +0100

    Adds Admin model

---
+++ PETDataBase.Domain/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PETDataBase.Domain.Models
+{
+    public class Admin : Agent
+    {
+        public Admin(string userName, string password) : base(userName, password)
+        {
+        }
+    }
+}
